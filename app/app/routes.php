e6e9a2f1818b806a1241335315493d79ae9f219e
A.J. Zwiers <azwiers@AJs-MacBook-Air.local>
2016-02-09 11:35:53 +0100

    Language selection

--- app/app/routes.php
+++ app/app/routes.php
@@ -2,6 +2,23 @@
    
 $apiRoot = "http://api.qcommerz.nl/";
  
+ 
+/**
+ * Before
+ */
+$app->hook('slim.before.dispatch', function() use ($app) { 
+    session_start();
+    if (empty($_SESSION['lang'])) {
+        $_SESSION['lang'] = 'nl';
+    }
+    $lang = $app->request()->params('lang');
+    if (isset($lang) && in_array($lang, array('nl', 'fr','en'))) {
+        $_SESSION['lang'] = $lang;
+    }
+    
+});
+
+
 /**
  * Home
  */
