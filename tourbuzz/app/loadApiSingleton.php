63b332691a7af845080e742e568a2fb51ccbea52
www admin <www-admin@joost.colo.transip.net>
2016-03-02 17:26:27 +0100

    guzzle in routes.php and htaccess more specific

---
+++ tourbuzz/app/loadApiSingleton.php
@@ -0,0 +1,47 @@
+<?php
+
+//FIXME move this class to own file.
+class ApiClient {
+    private $_guzzle;
+    private $_apiRoot;
+    
+    public function __construct($url) {
+        $this->_guzzle = new \GuzzleHttp\Client();
+        $this->_apiRoot = $url;
+    }
+    
+    public function getApiRoot() {
+        return $this->_apiRoot;
+    }
+    
+    public function get($uri) {
+        $requestUri = "{$this->_apiRoot}{$uri}";
+        $res = $this->_guzzle->request('GET', $requestUri);
+        return json_decode($res->getBody(), true);
+    }
+        
+    public function post($uri, $fields) {
+        $requestUri = "{$this->_apiRoot}{$uri}";
+        $this->_guzzle->request('POST', $requestUri, [
+            'form_params' => $fields
+        ]);
+    }
+    
+    public function delete($uri, $ids) {
+        $ids = $ids ? $ids : [];
+        
+        //url-ify the data for the POST
+        $fieldsString = '';
+        foreach($ids as $id) { $fieldsString .= 'ids[]='.$id.'&'; }
+        $fieldsString = rtrim($fieldsString, '&');
+
+        $requestUri = "{$this->_apiRoot}{$uri}?{$fieldsString}";
+
+        $this->_guzzle->request('DELETE', $requestUri);
+    }
+}
+
+$app->container->singleton('api', function () use ($apiRoot) {
+  return new ApiClient($apiRoot);
+});
+ 
--- tourbuzz/app/routes.php
+++ tourbuzz/app/routes.php
@@ -10,76 +10,8 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Message\Request;
 use GuzzleHttp\Message\Response;
 
-//FIXME move this class to own file.
-class ApiClient {
-    private $_guzzle;
-    private $_apiRoot;
-    
-    public function __construct($url) {
-        $this->_guzzle = new \GuzzleHttp\Client();
-        $this->_apiRoot = $url;
-    }
-    
-    public function getApiRoot() {
-        return $this->_apiRoot;
-    }
-    
-    public function get($uri) {
-        $res = $this->_guzzle->request('GET', $this->_apiRoot . "{$uri}/");
-        return json_decode($res->getBody(), true);
-    }
-        
-    public function post($uri, $fields) {
-        //FIXME use Guzzle here!
-    
-        //url-ify the data for the POST
-        $fields_string = '';
-        foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-        $fields_string = rtrim($fields_string, '&');
-    
-        //open connection
-        $ch = curl_init();
-        
-        //set the url, number of POST vars, POST data
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);        
-        curl_setopt($ch,CURLOPT_URL, $this->_apiRoot . "{$uri}/");
-        curl_setopt($ch,CURLOPT_POST, count($fields));
-        curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-        
-        //execute post
-        $result = curl_exec($ch);
-        $message = json_decode($result);
+require_once("loadApiSingleton.php");
 
-        //close connection
-        curl_close($ch);
-    }
-    
-    public function delete($uri, $ids) {
-        $ids = $ids ? $ids : [];
-        
-        //url-ify the data for the POST
-        $fields_string = '';
-        foreach($ids as $id) { $fields_string .= 'ids[]='.$id.'&'; }
-        $fields_string = rtrim($fields_string, '&');
-
-        $ch = curl_init();
-        
-        //set the url, number of POST vars, POST data
-        curl_setopt($ch,CURLOPT_URL, $this->_apiRoot . "{$uri}/" . "?" . $fields_string);
-        curl_setopt($ch,CURLOPT_CUSTOMREQUEST, "DELETE");
-        
-        //execute post
-        $result = curl_exec($ch);
-        
-        //close connection
-        curl_close($ch);
-    }
-}
-
-$app->container->singleton('api', function () use ($apiRoot) {
-  return new ApiClient($apiRoot);
-});
- 
 /**
  * Before
  */
@@ -88,7 +20,7 @@ $app->hook('slim.before', function() use ($app) {
         $_SESSION['lang'] = 'nl';
     }
     $lang = $app->request()->params('lang');
-    if (isset($lang) && in_array($lang, array('nl', 'fr', 'en'))) {
+    if (isset($lang) && in_array($lang, array('nl', /*'fr',*/ 'en'))) {
         $_SESSION['lang'] = $lang;
     }
 });
@@ -135,7 +67,7 @@ $app->get('/dashboard/login', function () use ($apiRoot) {
  */ 
 $app->get('/dashboard/berichten', function () use ($app, $image_api) {  
     
-    $berichten = $app->api->get("berichten/");
+    $berichten = $app->api->get("berichten");
     
     $data = [
         "berichten" => $berichten['messages'], 
@@ -156,12 +88,12 @@ $app->post('/dashboard/berichten', function () use ($app, $image_api) {
         'category' => $app->request->post('category'),
       	'title' => $app->request->post('title'),
       	'body' => $app->request->post('body'),
-      	'advice' => $app->request->post('advice'),      	
+      	'advice' => $app->request->post('advice'),
       	'title_en' => $app->request->post('title_en'),
       	'body_en' => $app->request->post('body_en'),
-        'advice_en' => $app->request->post('advice_en'),      	
+        'advice_en' => $app->request->post('advice_en'),
       	'title_fr' => $app->request->post('title_fr'),
-      	'body_fr' => $app->request->post('body_fr'),    	    	
+      	'body_fr' => $app->request->post('body_fr'),
       	'startdate' => $app->request->post('startdate'),
       	'enddate' => $app->request->post('enddate'),
       	'id' => $app->request->post('id'),
@@ -172,12 +104,12 @@ $app->post('/dashboard/berichten', function () use ($app, $image_api) {
     if ( empty ($fields['title']) ) {
         $app->flashNow('error', 'Titel is niet ingevuld');
     
-        $berichten = $app->api->get("berichten/");
+        $berichten = $app->api->get("berichten");
         
         $data = [
             "berichten" => $berichten['messages'], 
             "bericht" => $fields,
-            "image_api" => $image_api,     
+            "image_api" => $image_api,
             "api" => $app->api->getApiRoot(),
             "template" => "dashboard/berichten.twig",
         ];
@@ -186,7 +118,7 @@ $app->post('/dashboard/berichten', function () use ($app, $image_api) {
 
     } else {
     
-        $app->api->post("berichten/", $fields);
+        $app->api->post("berichten", $fields);
         
         $app->flash('success', 'Bericht toegevoegd');    
         $app->redirect("/dashboard/berichten");    
@@ -201,7 +133,7 @@ $app->post('/dashboard/berichten', function () use ($app, $image_api) {
  */ 
 $app->get('/dashboard/berichten/:id', function ($id) use ($app, $image_api) {
     
-    $berichten = $app->api->get("berichten/");
+    $berichten = $app->api->get("berichten");
     
     $data = [
         "test" => "world",
@@ -222,7 +154,7 @@ $app->get('/dashboard/berichten/:id', function ($id) use ($app, $image_api) {
 $app->post('/dashboard/berichten/verwijderen', function () use ($app) {
 
     $ids = $app->request->post('ids');
-    $berichten = $app->api->delete("berichten/", $ids);
+    $berichten = $app->api->delete("berichten", $ids);
     
     $app->flash('success', 'Bericht(en) verwijderd');        
     $app->redirect("/dashboard/berichten");
