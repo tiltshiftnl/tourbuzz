cb720a60a159c70c879bab5610a15c3191c86953
A.J. Zwiers <azwiers@AJs-MacBook-Air.local>
2016-03-03 10:49:09 +0100

    Maps bij berichten als locatie bekend is

--- tourbuzz/app/routes.php
+++ tourbuzz/app/routes.php
@@ -1,5 +1,5 @@
-<?php 
-   
+<?php
+
 require_once("config/config.php");
 $localConfigFilePath = __DIR__ . "/config/config_local.php";
 if (file_exists($localConfigFilePath)) {
@@ -15,7 +15,7 @@ require_once("loadApiSingleton.php");
 /**
  * Before
  */
-$app->hook('slim.before', function() use ($app) { 
+$app->hook('slim.before', function() use ($app) {
     if (empty($_SESSION['lang'])) {
         $_SESSION['lang'] = 'nl';
     }
@@ -30,31 +30,31 @@ $app->hook('slim.before', function() use ($app) {
  * Home
  */
 $app->get('/', function () use ($app, $apiRoot) {
-    
+
     if ( !empty($_SESSION['firstvisit']) ) {
         //$app->redirect(date('/Y/m/d'));
     }
-    
+
     $_SESSION['firstvisit'] = true;
-    
+
     $data = [
         "redirect" => date('/Y/m/d'),
         "template" => "splash.twig",
     ];
-    
-    render($data['template'], $data); 
+
+    render($data['template'], $data);
 });
 
 
 /*****************
 /* Admin Routes
  ****************/
- 
+
 /**
  * Login
- */ 
+ */
 $app->get('/dashboard/login', function () use ($apiRoot) {
-    
+
     $data = [
         "template" => "dashboard/login.twig",
     ];
@@ -64,26 +64,26 @@ $app->get('/dashboard/login', function () use ($apiRoot) {
 
 /**
  * Berichten get
- */ 
-$app->get('/dashboard/berichten', function () use ($app, $image_api) {  
-    
+ */
+$app->get('/dashboard/berichten', function () use ($app, $image_api) {
+
     $berichten = $app->api->get("berichten");
-    
+
     $data = [
-        "berichten" => $berichten['messages'], 
+        "berichten" => $berichten['messages'],
         "image_api" => $image_api,
         "api" => $app->api->getApiRoot(),
         "template" => "dashboard/berichten.twig",
     ];
-    
+
     render($data['template'], $data);
 })->name("berichten");
 
 /**
  * Berichten post
- */ 
+ */
 $app->post('/dashboard/berichten', function () use ($app, $image_api) {
-    
+
     $fields = array(
         'category' => $app->request->post('category'),
       	'title' => $app->request->post('title'),
@@ -100,46 +100,46 @@ $app->post('/dashboard/berichten', function () use ($app, $image_api) {
         'link' => $app->request->post('link'),
         'image_url' => $app->request->post('image_url'),
     );
-        
+
     if ( empty ($fields['title']) ) {
         $app->flashNow('error', 'Titel is niet ingevuld');
-    
+
         $berichten = $app->api->get("berichten");
-        
+
         $data = [
-            "berichten" => $berichten['messages'], 
+            "berichten" => $berichten['messages'],
             "bericht" => $fields,
             "image_api" => $image_api,
             "api" => $app->api->getApiRoot(),
             "template" => "dashboard/berichten.twig",
         ];
-        
+
         render($data['template'], $data);
 
     } else {
-    
+
         $app->api->post("berichten", $fields);
-        
-        $app->flash('success', 'Bericht toegevoegd');    
-        $app->redirect("/dashboard/berichten");    
-        
+
+        $app->flash('success', 'Bericht toegevoegd');
+        $app->redirect("/dashboard/berichten");
+
     }
-        
+
 })->name("berichten");
 
 
 /**
  * Berichten bewerken
- */ 
+ */
 $app->get('/dashboard/berichten/:id', function ($id) use ($app, $image_api) {
-    
+
     $berichten = $app->api->get("berichten");
-    
+
     $data = [
         "test" => "world",
         "bericht" => $berichten['messages'][$id],
         "berichten" => $berichten['messages'],
-        "image_api" => $image_api,   
+        "image_api" => $image_api,
         "api" => $app->api->getApiRoot(),
         "template" => "dashboard/berichten.twig",
     ];
@@ -150,36 +150,36 @@ $app->get('/dashboard/berichten/:id', function ($id) use ($app, $image_api) {
 
 /**
  * Berichten verwijderen
- */ 
+ */
 $app->post('/dashboard/berichten/verwijderen', function () use ($app) {
 
     $ids = $app->request->post('ids');
     $berichten = $app->api->delete("berichten", $ids);
-    
-    $app->flash('success', 'Bericht(en) verwijderd');        
+
+    $app->flash('success', 'Bericht(en) verwijderd');
     $app->redirect("/dashboard/berichten");
-    
+
 });
 
 /**
  * Dag
  */
 $app->get('/:y/:m/:d', function ($y, $m, $d) use ($app, $analytics, $image_api) {
-    
+
     $berichten = $app->api->get("berichten/{$y}/{$m}/{$d}");
-        
+
     $volgende = "/".str_replace('-', '/', $berichten['_nextDate']);
     $vorige   = "/".str_replace('-', '/', $berichten['_prevDate']);
-    
+
     $cruisekalender = $app->api->get("cruisekalender/{$y}/{$m}/{$d}");
 
     $N = date('N', strtotime("{$y}-{$m}-{$d}"));
-    
+
     $day = array (
         'ma', 'di', 'wo', 'do', 'vr', 'za', 'zo'
     );
-    
-    $dag = translate($day[(int)$N - 1]);    
+
+    $dag = translate($day[(int)$N - 1]);
 
     $data = [
         "lang" => $_SESSION['lang'],
@@ -192,11 +192,11 @@ $app->get('/:y/:m/:d', function ($y, $m, $d) use ($app, $analytics, $image_api) 
         "m" => $m,
         "y" => $y,
         "api" => $app->api->getApiRoot(),
-        "image_api" => $image_api,        
+        "image_api" => $image_api,
         "analytics" => $analytics,
         "cruisekalender" => $cruisekalender['items'],
         //"werkzaamheden" => $wegwerkzaamheden['werkzaamheden'],
-        //"evenementen" => $evenementen['evenementen'],            
+        //"evenementen" => $evenementen['evenementen'],
         "template" => "home.twig",
     ];
     render($data['template'], $data);
