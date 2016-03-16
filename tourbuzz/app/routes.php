af2960b3b7adac94fd3218869ef8f8cdef968e32
A.J. Zwiers <azwiers@AJs-MacBook-Air.local>
2016-03-16 16:15:17 +0100

    Maps met custom markers en links

--- tourbuzz/app/routes.php
+++ tourbuzz/app/routes.php
@@ -49,50 +49,73 @@ $app->get('/', function () use ($app, $apiRoot) {
 
 
 /**
- * Haltes
- */
-$app->get('/haltes', function () use ($app, $apiRoot) {
-
-    $haltes = $app->api->get("haltes");
-
-    $res = $app->api->get("berichten/".date('Y')."/".date('m')."/".date('d'));
-
-    $berichten = array_filter($res['messages'], function ($bericht) {
-        return !empty($bericht['is_live']);
-    });
-
-    $data = [
-        "berichten" => $berichten,
-        "haltes" => $haltes['haltes'],
-        "d" => date('d'),
-        "m" => date('m'),
-        "Y" => date('Y'),
-        "template" => "haltes.twig",
-    ];
-
-    render($data['template'], $data);
-});
-
-
-/**
  * Parkeerplaatsen
  */
 $app->get('/parkeren', function () use ($app, $apiRoot) {
 
-    $parkeerplaatsen = $app->api->get("parkeerplaatsen");
-
     $res = $app->api->get("berichten/".date('Y')."/".date('m')."/".date('d'));
 
     $berichten = array_filter($res['messages'], function ($bericht) {
         return !empty($bericht['is_live']);
     });
 
+    // Pixels per dLat and dLng for Amsterdam (approx) at zoomlevels.
+    $ppd = [
+        "lat" => [
+            "12" => 4770.821985494679,
+            "13" => 9541.643998239988,
+            "14" => 19083.28801010367,
+        ],
+        "lng" => [
+            "12" => 2912.711111111111,
+            "13" => 5825.422222222222,
+            "14" => 11650.844444444445,
+        ],
+    ];
+
+    $res = $app->api->get("parkeerplaatsen");
+    $parkeerplaatsen = $res['parkeerplaatsen'];
+
+    $center = [
+        "lat" => 52.372981,
+        "lng" => 4.901327,
+    ];
+
+    $mapOptions = [
+        "width" => 420,
+        "height" => 350,
+        "zoom" => 12,
+        "scale" => 2,
+    ];
+
+    // Calculate relative positions on the map.
+    $parkeerplaatsen = array_map(function ($parkeerplaats) use($center, $mapOptions, $ppd) {
+        $dLat = $parkeerplaats['location']['lat'] - $center['lat'];
+        $dLng = $parkeerplaats['location']['lng'] - $center['lng'];
+        $dY = $dLat * 100 * $ppd['lat'][$mapOptions['zoom']];
+        $dX = $dLng * 100 * $ppd['lng'][$mapOptions['zoom']];
+        $parkeerplaats['rel_loc'] = [
+            "dX" => 50 + ($dX / ($mapOptions['width'])),
+            "dY" => 50 - ($dY / ($mapOptions['height'])),
+        ];
+        return $parkeerplaats;
+    }, $parkeerplaatsen);
+
+    $parkeerplaatsen = array_filter($parkeerplaatsen, function ($parkeerplaats) {
+        return
+            !($parkeerplaats['rel_loc']['dX'] > 100) &&
+            !($parkeerplaats['rel_loc']['dX'] < 0) &&
+            !($parkeerplaats['rel_loc']['dY'] > 100) &&
+            !($parkeerplaats['rel_loc']['dY'] < 0);
+    });
+
     $data = [
-        "berichten" => $berichten,
-        "parkeerplaatsen" => $parkeerplaatsen['parkeerplaatsen'],
+        "parkeerplaatsen" => $parkeerplaatsen,
         "d" => date('d'),
         "m" => date('m'),
         "Y" => date('Y'),
+        "center" => $center,
+        "map" => $mapOptions,
         "template" => "parkeerplaatsen.twig",
     ];
 
@@ -387,6 +410,57 @@ $app->get('/:y/:m/:d', function ($y, $m, $d) use ($app, $analytics, $image_api) 
 
     $dag = translate($day[(int)$N - 1]);
 
+
+   // Pixels per dLat and dLng for Amsterdam (approx) at zoomlevels.
+    $ppd = [
+        "lat" => [
+            "12" => 4770.821985494679,
+            "13" => 9541.643998239988,
+            "14" => 19083.28801010367,
+        ],
+        "lng" => [
+            "12" => 2912.711111111111,
+            "13" => 5825.422222222222,
+            "14" => 11650.844444444445,
+        ],
+    ];
+
+   // $res = $app->api->get("haltes");
+   //$haltes = $res['haltes'];
+
+    $center = [
+        "lat" => 52.372981,
+        "lng" => 4.901327,
+    ];
+
+    $mapOptions = [
+        "width" => 420,
+        "height" => 350,
+        "zoom" => 12,
+        "scale" => 2,
+    ];
+
+    // Calculate relative positions on the map.
+    $berichten = array_map(function ($bericht) use($center, $mapOptions, $ppd) {
+        $dLat = $bericht['location']['lat'] - $center['lat'];
+        $dLng = $bericht['location']['lng'] - $center['lng'];
+        $dY = $dLat * 100 * $ppd['lat'][$mapOptions['zoom']];
+        $dX = $dLng * 100 * $ppd['lng'][$mapOptions['zoom']];
+        $bericht['rel_loc'] = [
+            "dX" => 50 + ($dX / ($mapOptions['width'])),
+            "dY" => 50 - ($dY / ($mapOptions['height'])),
+        ];
+        return $bericht;
+    }, $berichten);
+
+    $berichten = array_filter($berichten, function ($bericht) {
+        return
+            !($bericht['rel_loc']['dX'] > 100) &&
+            !($bericht['rel_loc']['dX'] < 0) &&
+            !($bericht['rel_loc']['dY'] > 100) &&
+            !($bericht['rel_loc']['dY'] < 0);
+    });
+
     $data = [
         "lang" => $_SESSION['lang'],
         "berichten" => $berichten,
@@ -402,15 +476,23 @@ $app->get('/:y/:m/:d', function ($y, $m, $d) use ($app, $analytics, $image_api) 
         "analytics" => $analytics,
         "cruisekalender" => $cruisekalender['items'],
         "timestamp" => $res['_timestamp'],
+        "center" => $center,
+        "map" => $mapOptions,
         "template" => "home.twig",
     ];
     render($data['template'], $data);
 })->name("home");
 
 /**
- * Testkaart
+ * Haltes
  */
-$app->get('/testkaart', function () use ($app, $apiRoot) {
+$app->get('/haltes', function () use ($app, $apiRoot) {
+
+    $res = $app->api->get("berichten/".date('Y')."/".date('m')."/".date('d'));
+
+    $berichten = array_filter($res['messages'], function ($bericht) {
+        return !empty($bericht['is_live']);
+    });
 
     // Pixels per dLat and dLng for Amsterdam (approx) at zoomlevels.
     $ppd = [
@@ -464,9 +546,12 @@ $app->get('/testkaart', function () use ($app, $apiRoot) {
 
     $data = [
         "haltes" => $haltes,
+        "d" => date('d'),
+        "m" => date('m'),
+        "Y" => date('Y'),
         "center" => $center,
         "map" => $mapOptions,
-        "template" => "testkaart.twig",
+        "template" => "haltes.twig",
     ];
 
     render($data['template'], $data);
