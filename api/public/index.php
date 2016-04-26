47acdb2ea7a36ebce2b97a86341df07de9698a0e
www admin <www-admin@joost.colo.transip.net>
2016-04-26 12:18:22 +0200

    api index

--- api/public/index.php
+++ api/public/index.php
@@ -1,50 +1,43 @@
 <?php
 
+
+
 header("Content-type: application/json");
 echo json_encode([
 	"apps" => [
 		"tourbuzz" => [
-			"url" => "http://buzz.fixxx.nl"
+			"url" => "http://www.tourbuzz.nl"
 		],
-		"haltecoach" => [
-			"url" => "http://coach.fixxx.nl"
-		]
 	],
 	"apis" => [
-		"attracties" => [
-			"origineel" => "http://www.amsterdamopendata.nl/files/Attracties.json",
-			"uri" => "http://api.fixxx.nl/attracties/"
+		"berichten" => [
+			"uri" => "http://api.tourbuzz.nl/berichten",
+            "voorbeeld" => "http://api.tourbuzz.nl/berichten/2016/03/02",
 		],
 		"haltes" => [
 			"origineel" => "http://www.amsterdamopendata.nl/files/ivv/touringcar/in_uitstaphaltes.json",
-			"uri" => "http://api.fixxx.nl/haltes/"
+			"uri" => "http://api.tourbuzz.nl/haltes/"
 		],
 		"parkeerplaatsen" => [
 			"origineel" => "http://www.fixxx.nl/parkeerplaatsen/",
-			"uri" => "http://api.fixxx.nl/parkeerplaatsen/"
+			"uri" => "http://api.tourbuzz.nl/parkeerplaatsen/"
 		],
 		"poi" => [
-			"uri" => "http://api.fixxx.nl/poi/",
-			"ov2" => "http://api.fixxx.nl/ov2/",
+			"uri" => "http://api.tourbuzz.nl/poi/",
+			"ov2" => "http://api.tourbuzz.nl/ov2/",
 			"maps" => "https://www.google.com/maps/d/viewer?mid=z2EXMMBPPl7c.kKS6guMeeUaI"
 		],
-		"afstanden" => [
-			"uri" => "http://api.fixxx.nl/distance/"
-		],
-		"pta" => [
-			"uri" => "http://api.fixxx.nl/pta/?m=1",
-			"arrivals" => "http://api.fixxx.nl/cruisekalender/2016/03/31"
-		],
-		"wegwerkzaamheden" => [
-			"origineel" => "http://www.amsterdamopendata.nl/files/Projecten_Amsterdam_GeoJson.json",
-			"uri" => "http://api.fixxx.nl/wegwerkzaamheden/2016/03/02"
-		],
-		"evenementen" => [
-			"origineel" => "http://www.amsterdamopendata.nl/files/Evenementen.json",
-			"uri" => "http://api.fixxx.nl/evenementen/2016/03/02"
-		],
-		"berichten" => [
-			"uri" => "http://api.fixxx.nl/berichten/2016/03/02"
-		]
+		//"pta" => [
+		//	"uri" => "http://api.fixxx.nl/pta/?m=1",
+		//	"arrivals" => "http://api.fixxx.nl/cruisekalender/2016/03/31"
+		//],
+		//"wegwerkzaamheden" => [
+		//	"origineel" => "http://www.amsterdamopendata.nl/files/Projecten_Amsterdam_GeoJson.json",
+		//	"uri" => "http://api.fixxx.nl/wegwerkzaamheden/2016/03/02"
+		//],
+		//"evenementen" => [
+		//	"origineel" => "http://www.amsterdamopendata.nl/files/Evenementen.json",
+		//	"uri" => "http://api.fixxx.nl/evenementen/2016/03/02"
+		//],
 	]
 ]);
