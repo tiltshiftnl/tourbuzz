a4571c185571c0681b6da612c27e8243d8c97720
www admin <www-admin@joost.colo.transip.net>
2016-05-10 16:30:50 +0200

    public routing

--- tourbuzz/public/routes.php
+++ tourbuzz/public/routes.php
@@ -1,27 +1,8 @@
 <?php
 
-/**
- * Whitelisting.
- */
-$app->hook('slim.before.dispatch', function () use ($app) {
-    $ips = [
-        '/^84\.241\.210\.169$/', // IP whitelist voorbeeld
-        '/^127\.0\.0\.1$/',
-	];
-	$matches = array_filter($ips, function ($ip) {
-		return preg_match($ip, $_SERVER["REMOTE_ADDR"]);
-	});
-	/*if (!$matches) {
-		$data = [
-			"ip" => $_SERVER["REMOTE_ADDR"]
-		];
-		render("whitelist.twig", $data);
-		$app->stop();
-	}*/
-});
 
 /**
- * Custom 404 pagina.
+ * Custom 404 page.
  */
 $app->notFound(function () use ($app) {
     $app->render("404.twig");
@@ -32,6 +13,7 @@ $app->notFound(function () use ($app) {
  * Robots.txt
  */
 $app->get('/robots.txt', function () use ($app) {
+
     header("Content-type: text/plain");
     if ( $_SERVER['HTTP_HOST'] == 'www.tourbuzz.nl' ) {
         readfile('robots.live.txt');
@@ -43,10 +25,10 @@ $app->get('/robots.txt', function () use ($app) {
 
 
 /**
- * Render wrapper om debug info te genereren.
- * FIXME Is er een andere manier om dit te doen?
+ * Render wrapper to add debug utilities.
  */
 function render($template, $data = [], $headers = []) {
+
     global $app;
     $data['current'] = $app->router()->getCurrentRoute()->getName();
 
@@ -57,26 +39,6 @@ function render($template, $data = [], $headers = []) {
         } else {
             $data['grid'] = 12; // default
         }
-    }
-
-    if (isset($_GET['sessiondestroy'])) {
-
-        // Unset all of the session variables.
-        $_SESSION = array();
-
-        // If it's desired to kill the session, also delete the session cookie.
-        // Note: This will destroy the session, and not just the session data!
-        if (ini_get("session.use_cookies")) {
-            $params = session_get_cookie_params();
-            setcookie(session_name(), '', time() - 42000,
-                $params["path"], $params["domain"],
-                $params["secure"], $params["httponly"]
-            );
-        }
-
-        // Finally, destroy the session.
-        session_destroy();
-        die('Session destroyed<br><a href="/">home</a>');
     }
 
     if (isset($_GET['debug'])) {
@@ -93,6 +55,7 @@ function render($template, $data = [], $headers = []) {
         }
     }
 
+    // Add custom headers to response.
     foreach ($headers as $key => $value) {
         $app->response->headers->set($key, $value);
     }
@@ -100,12 +63,11 @@ function render($template, $data = [], $headers = []) {
     $app->render($template, $data);
 }
 
+
 /**
- * Afbeeldingen schalen via externe url.
+ * Scale images on demand.
  */
 $app->get('/image/:operation/:width/:height', function ($operation, $width, $height) use ($app) {
-
-    //die('deze local?');
 
     $imageManager = new \Intervention\Image\ImageManager([
         "driver" => "imagick"
@@ -122,50 +84,24 @@ $app->get('/image/:operation/:width/:height', function ($operation, $width, $hei
     if ($height < 1) $height = null;
 
     $image = $imageManager->make($src)->{$operation}($width, $height);
-    echo $image->response('jpg', 85);
-    die;
+    exit($image->response('jpg', 85));
 });
 
-/**
- * Afbeeldingen schalen.
- */
-$app->get('/image/:operation/:width/:height/:path+', function ($operation, $width, $height, $filepath) {
-
-
-    $imageManager = new \Intervention\Image\ImageManager([
-        "driver" => "imagick"
-    ]);
-    if (!in_array($operation, ['resize', 'fit'])) {
-        die;
-    }
-
-    $filepath = implode("/", $filepath);
-    if (!file_exists($filepath)) {
-        die;
-    }
-    $width = (int) $width;
-    if ($width < 1) $width = null;
-    $height = (int) $height;
-    if ($height < 1) $height = null;
-    $image = $imageManager->make($filepath)->{$operation}($width, $height);
-
-    echo $image->response('jpg', 85);
-    die;
-});
 
 /**
- * Scss compileren.
+ * Compile scss files to css format.
  */
 $app->get('/css/:path+', function ($filepath) {
+
     $filepath = implode("/", $filepath);
     if (!file_exists($filepath)) {
         die;
     }
+
     $scssCompiler = new scssc();
     $scssCompiler->setFormatter("scss_formatter");
 
     header("Content-type: text/css");
-    echo $scssCompiler->compile("@import '{$filepath}'");
-    die;
-})->name("scss");
+    exit($scssCompiler->compile("@import '{$filepath}'"));
+});
 
