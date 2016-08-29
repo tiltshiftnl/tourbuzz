e147c2b1624fe6e6d01eee857db86da5abf33a6d
Bart Elferink <bart@soepel.it>
2016-08-29 11:57:47 +0200

    added dist config

---
+++ tourbuzz/app/config/config.dist.php
@@ -0,0 +1,8 @@
+<?php
+
+$apiRoot = "http://api.tourbuzz.nl/";
+$analytics = true;
+$image_api = "http://api.fixxx.nl/afbeeldingen";
+$mailTo = "m.sloothaak@amsterdam.nl, thartevelt@amsterdam.nl";
+$buzzProc = "http://";
+$buzzUri = "www.tourbuzz.nl";
