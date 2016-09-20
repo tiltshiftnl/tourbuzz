88b28e23162607a46e1c6ef10a21df5c671f769f
Bart Elferink <bart@soepel.it>
2016-09-20 12:49:59 +0200

    offline with service workers

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Tour Buzz
+
+For testing with the service worker use the following command on mac:
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir=/some/data/dir --unsafely-trt-insecure-origin-as-secure=http://your.site.url
---
+++ tourbuzz/public/offline.js
@@ -0,0 +1,73 @@
+// A list of paths to cache
+var paths = [
+    '/img/buzzlogo-w.png',
+    '/img/tour-buzz.png',
+    '/img/taal/nl.png',
+    '/img/taal/de.png',
+    '/img/taal/en.png',
+    '/img/down-w.png',
+    '/img/markers/1.png',
+    '/img/markers/2.png',
+    '/img/markers/3.png',
+    '/img/markers/4.png',
+    '/img/markers/5.png',
+    '/img/markers/6.png',
+    '/img/markers/7.png',
+    '/img/markers/8.png',
+    '/img/markers/9.png',
+    '/img/markers/10.png',
+    '/img/markers/11.png',
+    '/img/markers/12.png',
+    '/img/markers/13.png',
+    '/img/markers/14.png',
+    '/img/markers/15.png',
+    '/fallback',
+    '/css/scss/main.scss'
+];
+
+//
+// // Set the callback for the install step
+// self.addEventListener('install', function(event) {
+//     // Open the cache (and name it)
+//     caches.open('offline').then(function(cache) {
+//         return cache.addAll(paths);
+//     });
+// });
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('offline')
+            .then(function(cache) {
+                return cache.addAll(paths);
+            })
+    );
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('fetch', function(event) {
+    var requestURL = new URL(event.request.url);
+
+    /**
+     * Wanneer de pagina niet in de cache array zit, kijken of deze online beschikbaar is.
+     * Is deze niet online beschikbaar de fallback pagina teruggeven
+     */
+    if (-1 == paths.indexOf(requestURL.pathname)) {
+        event.respondWith(
+            fetch(event.request).catch(function() {
+                return caches.match('/fallback');
+            })
+        );
+        return;
+    }
+
+    /**
+     * Kijk of het onderdeel al gecached is, zo niet haal het op van het internet
+     */
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+
+    console.log('done');
+});
