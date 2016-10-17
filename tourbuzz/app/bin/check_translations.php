b36df21ae327ece3151085c9d7dce16752ca2b13
Bart Elferink <bart@soepel.it>
2016-10-17 12:22:03 +0200

    check vertalingen

---
+++ tourbuzz/app/bin/check_translations.php
@@ -0,0 +1,20 @@
+<?php
+
+$translations = json_decode(file_get_contents('translations/translations.json'), true);
+
+$languages = ['en', 'de', 'es'];
+
+foreach ($translations['translations'] as $key => $arr) {
+    $missing = [];
+    foreach ($languages as $lang) {
+        if (!isset($arr[$lang])) {
+            $missing[] = $lang;
+        }
+    }
+
+    if (count($missing)) {
+        echo "--------------------------------------------------------------\n";
+        echo $key . "\n";
+        echo "Missende taal/talen: " . implode(", ", $missing) . "\n";
+    }
+}
