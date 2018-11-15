6916e98024bc2153560aecb27478e1ff1b692f4a
Stephan Zaat <s.zaat@amsterdam.nl>
2018-11-15 12:35:42 +0100

    Added dynamic .env file

--- Docker/docker-entrypoint.sh
+++ Docker/docker-entrypoint.sh
@@ -5,6 +5,11 @@ echo Starting server
 set -u
 set -e
 
+cat > /srv/web/tourbuzz/tourbuzz/.env <<EOF
+GOOGLEMAPS_API_KEY="${GOOGLEMAPS_API_KEY}"
+EOF
+
+
 tail -f /var/log/php7.0-fpm.log &
 
 service php7.0-fpm start
