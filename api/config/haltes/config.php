963752ab422aaab563d8d46556887e9ac0ec27e9
Maarten de Keizer <github@maartendekeizer.nl>
2019-03-23 11:45:33 +0100

    Remove old API from frontend repository

--- api/config/haltes/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$sourceUrl = "http://open.datapunt.amsterdam.nl/ivv/touringcar/in_uitstaphaltes.json";
-$messagesUrl = "http://api.tourbuzz.nl/berichten/" . date("Y/m/d");
-
--- api/config/parkeerplaatsen/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$sourceUrl = "http://open.datapunt.amsterdam.nl/ivv/touringcar/parkeerplaatsen.json";
-$messagesUrl = "http://api.tourbuzz.nl/berichten/" . date("Y/m/d");
