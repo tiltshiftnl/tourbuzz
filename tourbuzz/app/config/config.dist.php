10ccf80581302493aced0a0ae1b7cdfb17cce204
Maarten de Keizer <github@maartendekeizer.nl>
2019-04-13 12:34:04 +0200

    Remove e-mailaddresses from dist config

--- tourbuzz/app/config/config.dist.php
+++ tourbuzz/app/config/config.dist.php
@@ -3,6 +3,6 @@
 $apiRoot = "http://api.tourbuzz.nl/";
 $analytics = true;
 $image_api = "http://api.fixxx.nl/afbeeldingen";
-$mailTo = "m.sloothaak@amsterdam.nl, thartevelt@amsterdam.nl";
+$mailTo = "***";
 $buzzProc = "http://";
 $buzzUri = "www.tourbuzz.nl";
